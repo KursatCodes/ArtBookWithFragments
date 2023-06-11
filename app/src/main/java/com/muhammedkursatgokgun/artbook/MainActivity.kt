188c53ad9019de4d67b0b6d2a94af3308b3685ac
Muhammed <kgokgun94@gmail.com>
2023-06-11 13:54:40 +0300

    Sign in page complated.

--- app/src/main/java/com/muhammedkursatgokgun/artbook/MainActivity.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/MainActivity.kt
@@ -2,10 +2,17 @@ package com.muhammedkursatgokgun.artbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun signIn(view:View){
+
+    }
+    fun signUp(view:View){
+
     }
 }
