2d7e20ee74e9850f587f2eb012eea1df1f12f32d
Muhammed <kgokgun94@gmail.com>
2023-06-11 14:36:07 +0300

    Layouts design is complated.

---
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/activities/ArtActivity.kt
@@ -0,0 +1,12 @@
+package com.muhammedkursatgokgun.artbook.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhammedkursatgokgun.artbook.R
+
+class ArtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art)
+    }
+}
--- app/src/main/java/com/muhammedkursatgokgun/artbook/activities/MainActivity.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/activities/MainActivity.kt
@@ -1,13 +1,17 @@
-package com.muhammedkursatgokgun.artbook
+package com.muhammedkursatgokgun.artbook.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.muhammedkursatgokgun.artbook.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent= Intent(this,ArtActivity::class.java)
+        startActivity(intent)
     }
     fun signIn(view:View){
 
