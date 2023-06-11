68408b28da4bd77449ccc730d616d23f0e67f848
Muhammed <kgokgun94@gmail.com>
2023-06-11 15:04:16 +0300

    Fragment actions complated.

--- app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/ArtFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/ArtFragment.kt
@@ -5,19 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.muhammedkursatgokgun.artbook.R
+import com.muhammedkursatgokgun.artbook.databinding.FragmentArtBinding
 
+private var _binding: FragmentArtBinding?=null
+
+private val binding get() = _binding!!
 class ArtFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding= FragmentArtBinding.inflate(inflater,container,false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_art, container, false)
+        return view
+        //return inflater.inflate(R.layout.fragment_art, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonToUploadFr.setOnClickListener {
+            val action = ArtFragmentDirections.actionArtFragmentToUploadFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/UploadFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/UploadFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.muhammedkursatgokgun.artbook.R
+import com.muhammedkursatgokgun.artbook.databinding.FragmentUploadBinding
+
+private var _binding: FragmentUploadBinding?=null
+
+private val binding get() = _binding!!
 
 class UploadFragment : Fragment() {
 
@@ -16,7 +22,18 @@ class UploadFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding= FragmentUploadBinding.inflate(inflater,container,false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false)
+        return view
+        //return inflater.inflate(R.layout.fragment_upload, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUpload.setOnClickListener {
+            val action = UploadFragmentDirections.actionUploadFragmentToArtFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
