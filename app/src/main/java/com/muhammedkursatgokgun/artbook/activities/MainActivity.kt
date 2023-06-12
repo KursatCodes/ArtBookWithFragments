60617c8048b9a373a633000945f7c235708dd2a1
Muhammed <kgokgun94@gmail.com>
2023-06-12 07:40:46 +0300

    Cracked commit

--- app/src/main/java/com/muhammedkursatgokgun/artbook/activities/MainActivity.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/activities/MainActivity.kt
@@ -4,16 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.muhammedkursatgokgun.artbook.R
+import com.muhammedkursatgokgun.artbook.databinding.ActivityMainBinding
 
+private lateinit var auth: FirebaseAuth
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intent= Intent(this,ArtActivity::class.java)
-        startActivity(intent)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            val intent= Intent(this,ArtActivity::class.java)
+            startActivity(intent)
+        }
+
     }
     fun signIn(view:View){
+        var email = binding.edtTextEMail.text.toString()
+        var password = binding.edtTextPassword.text.toString()
 
     }
     fun signUp(view:View){
