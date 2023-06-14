f8d9d02475f15fc1226527dec52f18b052086b0b
Muhammed <kgokgun94@gmail.com>
2023-06-14 08:08:45 +0300

    Option menu added.

--- app/src/main/java/com/muhammedkursatgokgun/artbook/activities/ArtActivity.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/activities/ArtActivity.kt
@@ -1,7 +1,10 @@
 package com.muhammedkursatgokgun.artbook.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.muhammedkursatgokgun.artbook.R
 
 class ArtActivity : AppCompatActivity() {
--- app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/ArtFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/artbook/fragments/ArtFragment.kt
@@ -1,22 +1,60 @@
 package com.muhammedkursatgokgun.artbook.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.fragment.app.transaction
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.muhammedkursatgokgun.artbook.R
+import com.muhammedkursatgokgun.artbook.activities.MainActivity
 import com.muhammedkursatgokgun.artbook.databinding.FragmentArtBinding
+import kotlin.concurrent.fixedRateTimer
 
 private var _binding: FragmentArtBinding?=null
+private lateinit var auth: FirebaseAuth
 
 private val binding get() = _binding!!
 class ArtFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.art_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_art -> {
+                // Navigate to settings screen.
+                val action = ArtFragmentDirections.actionArtFragmentToUploadFragment()
+                Navigation.findNavController(this@ArtFragment.requireView()).navigate(action)
+                true
+            }
+            R.id.sign_out -> {
+                // Save profile changes.
+                auth.signOut()
+                val intent = Intent(this.context,MainActivity::class.java)
+                startActivity(intent)
+                fragmentManager?.commit {
+                    setReorderingAllowed(true)
+                    replace<UploadFragment>(R.id.artFragment)
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onCreateView(
@@ -37,5 +75,8 @@ class ArtFragment : Fragment() {
             val action = ArtFragmentDirections.actionArtFragmentToUploadFragment()
             Navigation.findNavController(it).navigate(action)
         }
+        auth = Firebase.auth
     }
+
+
 }
